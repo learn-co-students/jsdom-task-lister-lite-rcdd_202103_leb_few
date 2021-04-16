febcb907f8d149688f1663ff5f4d26cfd5bf33b4
Mahmoud Ajam <mahmoud.aajam@gmail.com>
2021-04-16 17:47:32 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  let txt = document.getElementById('new-task-description');
+  let btn = document.getElementsByTagName('input')[1];
+  let taskList = document.getElementById('tasks');
+  btn.addEventListener('click', fucntion(e){
+    taskList.innerHTML += `<li>${txt.textContent}</li>`;
+    event.preventDefault();
+  }
+}
