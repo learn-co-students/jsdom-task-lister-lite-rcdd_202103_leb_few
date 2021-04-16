fcfee2b900a83622daf30e12b0a083c533030afd
Mira Chami <miramc149@gmail.com>
2021-04-16 18:35:35 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let task = document.getElementById("new-task-description");
+  let submit = document.getElementsByTagName("input")[1];
+  let tasks = document.querySelector("#tasks");
+  submit.addEventListener("click",function(e) {
+   e.preventDefault();
+   let li = document.createElement("li");
+   li.innerHTML = task.value;
+   tasks.appendChild(li);
+
+  }
 });
