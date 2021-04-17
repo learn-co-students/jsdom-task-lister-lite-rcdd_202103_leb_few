c04df7e7bcaca56c1b1bb39755ad338d79ba250d
Adnan Sayed <adnan.sayed.official@gmail.com>
2021-04-17 05:12:53 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
 });
+
+
+
+let txt = document.getElementById('new-task-description');
+let todo=document.getElementById('list');
+let btn = document.getElementById("btn");
+
+document.getElementById("btn").addEventListener("click", function() {
+  addText(txt.value);
+});
+
+function addText(text)
+{
+
+  todo.innerHTML+=`<li>${text}</li>`;
+}
