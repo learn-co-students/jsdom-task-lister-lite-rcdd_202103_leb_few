3d82bfce32cd34a104ebe6f6798d0b3685009e22
Marwa  Awwad <marwaawwad10@gmail.com>
2021-04-28 23:12:27 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
+let l=[];
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+//Event.preventDefault();
+let newtask=document.getElementById('new-task-description');
+let u=document.getElementById('tasks');
+
+
+let b =document.getElementById('button');
+let form=document.getElementById('create-task-form')
+form.addEventListener("submit", function(e){
+  e.preventDefault();
+
+let LI=document.createElement("li");
+let B=document.createElement("button")
+LI.innerHTML+=newtask.value;
+B.innerHTML="Done";
+u.appendChild(LI);
+LI.appendChild(B);
+newtask.value="";
+console.log("DONE");
+B.addEventListener("click", function(e){
+  e.preventDefault();
+  LI.remove();
 });
+});});
